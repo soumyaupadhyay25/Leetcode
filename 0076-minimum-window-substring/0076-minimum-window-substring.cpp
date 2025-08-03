a0d61b462026b59f336b197b8b048bd48ecb097d
Soumya Upadhyay <131898899+soumyaupadhyay25@users.noreply.github.com>
2025-08-03 15:12:23 +0530

    Time: 2 ms (85.58%), Space: 11.2 MB (94.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,17 +1,19 @@
 class Solution {
  public:
   string minWindow(string s, string t) {
-    vector<int> count(128);
+    vector<int> count(128); // ASCII size
     int required = t.length();
     int bestLeft = -1;
     int minLength = s.length() + 1;
 
+    // Count each character in t
     for (const char c : t)
       ++count[c];
 
     for (int l = 0, r = 0; r < s.length(); ++r) {
       if (--count[s[r]] >= 0)
         --required;
+
       while (required == 0) {
         if (r - l + 1 < minLength) {
           bestLeft = l;
